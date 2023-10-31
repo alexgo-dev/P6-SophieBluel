2aed9163e44747c42d3503e37fe85ffef11bc7f7
foolzdev <foolvst@gmail.com>
2023-10-31 04:33:22 +0100

    ajout de la déconnexion et du bouton d'édition

--- FrontEnd/assets/admin.js
+++ FrontEnd/assets/admin.js
@@ -1,12 +1,22 @@
 async function admin() {
     const editBar = document.querySelector(".editBar");
     const login = document.getElementById("login");
+    const editFilters = document.querySelector(".filters");
 
     if (localStorage.getItem("token")) {
         editBar.style.display = "flex";
         login.textContent = "logout";
-    }
-    else {
+        editFilters.style.display = "none";
+
+        //logout
+
+        login.addEventListener("click", function (event) {
+            event.preventDefault();
+
+            localStorage.removeItem("token");
+            window.location.href = "index.html";
+        });
+    } else {
         console.log("problème lors de l'authentification");
     }
 }
